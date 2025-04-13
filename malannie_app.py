76d484fc7c053c38c000c3958c0aa3146b1c7ca9
Faouaghzene <faouaghzene@gmail.com>
2025-04-13 14:08:25 +0200

    Update malannie_app.py

--- malannie_app.py
+++ malannie_app.py
@@ -20,9 +20,11 @@ st.write ('yous smoothie  is :',name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe,use_container_width=True)
+#st.dataframe(data=my_dataframe,use_container_width=True)
+#st.stop
+pd_df = my_dataframe.to_pandas ()
+st.dataframe (pd_df)
 st.stop
-
 ingredients_list = st.multiselect ('Choose top 5 fruit',my_dataframe,max_selections=5)
 #st.write(ingredients_list)
 if ingredients_list :
