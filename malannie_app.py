46c081b454c98d6da496f5a67ac74379363e7f8f
Faouaghzene <faouaghzene@gmail.com>
2025-04-13 13:47:01 +0200

    Update malannie_app.py

--- malannie_app.py
+++ malannie_app.py
@@ -29,7 +29,8 @@ if ingredients_list :
     ingredients_string=''
     for choose_fruit in ingredients_list :
         ingredients_string += choose_fruit +' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader (choose_fruit  +'Nutrition Information'
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"+choose_fruit )
         sd = st.dataframe (smoothiefroot_response.json(),use_container_width=True)
 
         #st.write (ingredients_string)
