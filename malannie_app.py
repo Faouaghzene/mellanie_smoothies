2280d6ead726581eb39181f3fe2c0b02e83235cb
Faouaghzene <faouaghzene@gmail.com>
2025-04-13 14:16:26 +0200

    Update malannie_app.py

--- malannie_app.py
+++ malannie_app.py
@@ -32,8 +32,8 @@ if ingredients_list :
     ingredients_string=''
     for choose_fruit in ingredients_list :
         ingredients_string += choose_fruit +' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == choose_fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', choose_fruit,' is ', search_on, '.')
         st.subheader (choose_fruit  +' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"+choose_fruit )
         sd = st.dataframe (smoothiefroot_response.json(),use_container_width=True)
